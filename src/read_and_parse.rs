c96ce248eacd5f973f9bee55a6995cf63cd999e2
embrx <andrew8162003@gmail.com>
2024-05-05 17:31:28 -0400

    neatness

--- src/read_and_parse.rs
+++ src/read_and_parse.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Result};
+
 pub fn read_and_parse(filename: &str) -> Result<HashMap<i32, Vec<i32>>> {
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
